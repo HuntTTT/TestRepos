d556ed197806b614090ef5b5ca7369200c65aef4
Pavel <103637313+HuntTTT@users.noreply.github.com>
2023-09-17 14:02:31 +1000

    Добавил вместо 1 Бла Бла

--- Gollar.java
+++ Gollar.java
@@ -2,6 +2,6 @@ package TestRepos;
 
 public class Gollar {
     public static void main(String[] args) {
-        System.out.println("1");
+        System.out.println("Добавил вместо 1 Бла Бла");
     }
 }
